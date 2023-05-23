2eb6e236ae13b7f122e5a17247ebfcefb98cc7a1
scostilla <sebcostilla@gmail.com>
2023-05-23 12:55:59 -0300

    working on popup

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  styleUrls: ['./calendar.component.css'],
 })
-
-
 export class CalendarComponent {
-
-
+  @Input() isReadOnly: Boolean = false;
+  currentDate: string = new Date().toLocaleDateString('ea-AR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
 }
