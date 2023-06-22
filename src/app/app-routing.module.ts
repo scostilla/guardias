317fc17a74cadcbf4ac99b095446213eb33ca33d
Sebastián Rodrigo Costilla <sebcostilla@gmail.com>
2023-06-22 12:35:16 -0300

    schedule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProfessionalFormComponent } from './components/professional-form/professional-form.component';
 import { DailyScheduleComponent } from './components/daily-schedule/daily-schedule.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegistroDiarioComponent } from './components/registro-diario/registro-diario.component';
+import { ProfessionalListComponent } from './components/professional-list/professional-list.component';
+import { ProfessionalNewsComponent } from './components/professional-news/professional-news.component';
+import { ScheduleDistributionComponent } from './components/schedule-distribution/schedule-distribution.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'professional-form', component: ProfessionalFormComponent },
   { path: 'daily-schedule', component: DailyScheduleComponent},
-  {path:"home", component: HomeComponent},
-  {path:"login", component: LoginComponent},
-  {path:"registro-diario",component: RegistroDiarioComponent}
+  { path: 'professional-list', component: ProfessionalListComponent},
+  { path: 'professinal-news', component: ProfessionalNewsComponent},
+  { path: 'schedule-distribution', component: ScheduleDistributionComponent},
 ];
 
 @NgModule({
---
+++ src/app/components/professional-news/professional-news.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-news',
+  templateUrl: './professional-news.component.html',
+  styleUrls: ['./professional-news.component.css']
+})
+export class ProfessionalNewsComponent {
+
+}
