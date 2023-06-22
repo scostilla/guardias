2f6d7c900b96051bbd292571530c6c5647dd87a8
fabiana Yucgra <fabianay0024@gmail.com>
2023-06-22 13:14:39 -0300

    subo vista registro de actividades

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProfessionalFormComponent } from './components/professional-form/professional-form.component';
 import { DailyScheduleComponent } from './components/daily-schedule/daily-schedule.component';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroDiarioComponent } from './components/registro-diario/registro-diario.component';
+import { RegistroActividadesComponent } from './components/registro-actividades/registro-actividades.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: '', component: LoginComponent },
   { path: 'professional-form', component: ProfessionalFormComponent },
   { path: 'daily-schedule', component: DailyScheduleComponent},
-  {path:"home", component: HomeComponent},
-  {path:"login", component: LoginComponent},
-  {path:"registro-diario",component: RegistroDiarioComponent}
+  {path:"home-page", component:HomePageComponent},
+  {path:"registro-diario",component: RegistroDiarioComponent},
+  {path:"registro-actividades",component: RegistroActividadesComponent}
 ];
 
 @NgModule({
---
+++ src/app/components/registro-actividades/registro-actividades.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-actividades',
+  templateUrl: './registro-actividades.component.html',
+  styleUrls: ['./registro-actividades.component.css']
+})
+export class RegistroActividadesComponent {
+
+}
