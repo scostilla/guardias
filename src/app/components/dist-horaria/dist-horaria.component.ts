574c92fc8a10b21949f2b751ef6741b784e56284
Sebastián Rodrigo Costilla <sebcostilla@gmail.com>
2023-06-27 11:10:42 -0300

    hospital Lists

--- src/app/components/dist-horaria/dist-horaria.component.ts
+++ src/app/components/dist-horaria/dist-horaria.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dist-horaria',
   templateUrl: './dist-horaria.component.html',
-  styleUrls: ['./dist-horaria.component.css']
+  styleUrls: ['./dist-horaria.component.css'],
 })
 export class DistHorariaComponent {
-  selectedService:string='Cargo';
+  selectedService: string = 'Cargo';
   selectedGuard: string = '';
   disableButton: boolean = this.selectedGuard == '';
-  
-  
+  constructor(private http: HttpClient) {}
+  options: any[] | undefined;
+
+  ngOnInit() {
+    this.http
+      .get<any[]>('../assets/jsonFiles/hospitales.json')
+      .subscribe((data) => {
+        this.options = data;
+      });
+  }
 }
--- src/app/components/novedades-form/novedades-form.component.ts
+++ src/app/components/novedades-form/novedades-form.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-novedades-form',
   templateUrl: './novedades-form.component.html',
-  styleUrls: ['./novedades-form.component.css']
+  styleUrls: ['./novedades-form.component.css'],
 })
 export class NovedadesFormComponent {
-  selectedService:string='Compensatorio';
+  selectedService: string = 'Compensatorio';
   selectedGuard: string = '';
   disableButton: boolean = this.selectedGuard == '';
+  options: any[] | undefined;
 
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http
+      .get<any[]>('../assets/jsonFiles/hospitales.json')
+      .subscribe((data) => {
+        this.options = data;
+      });
+  }
 }
