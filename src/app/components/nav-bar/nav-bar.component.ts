6a8149458f059e3feeac9335328c93bec51a73d7
sebcostilla <sebcostilla@gmail.com>
2023-06-28 18:48:32 -0300

    navBar working

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,26 +1,11 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Router, NavigationStart } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnDestroy {
-  private routerSubscription: Subscription;
+export class NavBarComponent{
 
-  constructor(private router: Router) {
-    this.routerSubscription = this.router.events.subscribe((event) => {
-      if (event instanceof NavigationStart) {
-        console.log('url:', event.url);
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.routerSubscription) {
-      this.routerSubscription.unsubscribe();
-    }
-  }
 }
