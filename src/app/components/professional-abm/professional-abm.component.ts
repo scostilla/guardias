d3cd98ef0855b245d2939f76e294654f803a9ac9
Sebastián Rodrigo Costilla <sebcostilla@gmail.com>
2023-07-03 11:48:52 -0300

    .

--- src/app/components/professional-abm/professional-abm.component.ts
+++ src/app/components/professional-abm/professional-abm.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
-import { FormBuilder, FormGroup, Validators,FormControl  } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 interface Specialty {
   value: string;
@@ -14,9 +19,9 @@ interface Specialty {
   styleUrls: ['./professional-abm.component.css'],
 })
 export class ProfessionalAbmComponent {
-  selectedProfessional: string="Medico";
-  selectedRevista: string ="contratado";
-  selectedCarga: string="40";
+  selectedProfessional: string = 'Medico';
+  selectedRevista: string = 'contratado';
+  selectedCarga: string = '40';
   professionalForm: FormGroup;
   options: any[] | undefined;
 
@@ -53,10 +58,16 @@ export class ProfessionalAbmComponent {
     private fb: FormBuilder
   ) {
     this.professionalForm = this.fb.group({
-      nombre: ['', [Validators.required, Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ ]*')]],
-      apellido: ['', [Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ ]*'),Validators.required]],
-      dni: ['', [Validators.pattern('[0-9]*'),Validators.required]],
-      cuil: ['', [Validators.pattern('[0-9]*'),Validators.required]],
+      nombre: [
+        '',
+        [Validators.required, Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ ]*')],
+      ],
+      apellido: [
+        '',
+        [Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ ]*'), Validators.required],
+      ],
+      dni: ['', [Validators.pattern('[0-9]*'), Validators.required]],
+      cuil: ['', [Validators.pattern('[0-9]*'), Validators.required]],
       professional: ['', Validators.required],
       specialty: [''],
       revista: ['', Validators.required],
