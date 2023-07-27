b2cd1f4a6824777a447c9a3b22d5f8ee9a922b06
fabiana Yucgra <fabianay0024@gmail.com>
2023-07-27 12:35:20 -0300

    prueba

--- src/app/components/ddjj-cargoyagrup/ddjj-cargoyagrup.component.ts
+++ src/app/components/ddjj-cargoyagrup/ddjj-cargoyagrup.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./ddjj-cargoyagrup.component.css']
 })
 export class DdjjCargoyagrupComponent {
+  /* date = '2023-07-21T13:59:31.238Z';  */
+  
+  today:number = new Date(2023,7,0).getDate();//31
+  numberOfMonth: Array<number> = new Array<number>();
 
+  daysOfMonth: Array<Date> = new Array<Date>(); 
+  
+  constructor(){
+    for (var dia = 1; dia <= this.today; dia++) {
+      this.numberOfMonth.push(dia);
+    }
+
+    for (var di = 1; di <= this.numberOfMonth.length; di++) {
+      const currentDate = new Date();
+      const year = currentDate.getFullYear();
+      const month = currentDate.getMonth();
+      const especificDate = new Date (year,month,di)
+      this.daysOfMonth.push(especificDate);
+      
+    }
+  }
 }
