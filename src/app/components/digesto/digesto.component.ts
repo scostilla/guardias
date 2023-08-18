8093ae2997ba0670e86a350bd3f06666fe56a1df
ErgoMori <nikho_@hotmail.com>
2023-08-18 20:57:59 -0300

    cambios digesto

--- src/app/components/digesto/digesto.component.ts
+++ src/app/components/digesto/digesto.component.ts
@@ -4,6 +4,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
 export interface PeriodicElement {
+    guardia: string;
     cat: string;
     position: number;
     act: string;
@@ -11,10 +12,10 @@ export interface PeriodicElement {
     ver: string;
 }
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, cat: 'Circular', act: '24/07/2023', detalle: 'Circular N° 11: Ajustes guardias contra factura 1° y 2° corte - Mayo 2023', ver: '/assets/digesto/CIRCULAR-11-DPH-2023.pdf'},
-  {position: 2, cat: 'Circular', act: '17/07/2023', detalle: 'Circular N° 10: Rectificativa circular N° 2 - Guardias contra factura', ver: '/assets/digesto/CIRCULAR-10-2023.pdf'},
-  {position: 3, cat: 'Resolución conjunta', act: '17/07/2023', detalle: 'Resolución conjunta N° 114: necesidad de actualizar los valores del régimen de guardias brindadas en los establecimientos. Descongelamiento del valor de la Guardia Médica del Interior.', ver: '/assets/digesto/RESOL-CONJUNTA-114-SyHF-2023.pdf'},
-  {position: 4, cat: 'Resolución conjunta', act: '03/07/2023', detalle: 'Resolución conjunta N° 161: en referencia a la resolución conjunta N° 114, sobre el descongelamiento del valor de la Guardia Médica del Interior.', ver: '/assets/digesto/RESOL-CONJUNTA-161-S-2023.pdf'},
+  {guardia: 'guardia-act', position: 1, cat: 'Circular', act: '24/07/2023', detalle: 'Circular N° 11: Ajustes guardias contra factura 1° y 2° corte - Mayo 2023', ver: '/assets/digesto/CIRCULAR-11-DPH-2023.pdf'},
+  {guardia: 'guardia-act', position: 2, cat: 'Circular', act: '17/07/2023', detalle: 'Circular N° 10: Rectificativa circular N° 2 - Guardias contra factura', ver: '/assets/digesto/CIRCULAR-10-2023.pdf'},
+  {guardia: 'guardia-pas', position: 3, cat: 'Resolución conjunta', act: '17/07/2023', detalle: 'Resolución conjunta N° 114: necesidad de actualizar los valores del régimen de guardias brindadas en los establecimientos. Descongelamiento del valor de la Guardia Médica del Interior.', ver: '/assets/digesto/RESOL-CONJUNTA-114-SyHF-2023.pdf'},
+  {guardia: 'guardia-act', position: 4, cat: 'Resolución conjunta', act: '03/07/2023', detalle: 'Resolución conjunta N° 161: en referencia a la resolución conjunta N° 114, sobre el descongelamiento del valor de la Guardia Médica del Interior.', ver: '/assets/digesto/RESOL-CONJUNTA-161-S-2023.pdf'},
 ];
 
 
@@ -25,7 +26,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class DigestoComponent {
-  displayedColumns: string[] = ['position', 'cat', 'act', 'detalle', 'ver'];
+  displayedColumns: string[] = ['guardia', 'position', 'cat', 'act', 'detalle', 'ver'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
 
