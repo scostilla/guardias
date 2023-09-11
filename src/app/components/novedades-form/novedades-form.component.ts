cf4d4357c03658d7df86197cdabe695a6dcdcc80
sebcostilla <sebcostilla@gmail.com>
2023-09-11 12:01:25 -0300

    novedades con json

--- src/app/components/novedades-form/novedades-form.component.ts
+++ src/app/components/novedades-form/novedades-form.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 
@@ -7,9 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./novedades-form.component.css'],
 })
 export class NovedadesFormComponent {
-  hospitales:string[]= ['DN. PABLO SORIA'];
-  profesional:string[]= ['FIGUEROA	ELIO','ARRAYA	PEDRO ADEMIR','MORALES	RICARDO','ALFARO	FIDEL','MARTINEZ	YANINA VANESA G.'];
-  novedad:string[]=['Compensatorio','L.A.O.','Maternidad','Parte de Enfermo','Familiar Enfermo','Falta sin aviso']
+  //hospitales:string[]= ['DN. PABLO SORIA'];
+  //profesional:string[]= ['FIGUEROA	ELIO','ARRAYA	PEDRO ADEMIR','MORALES	RICARDO','ALFARO	FIDEL','MARTINEZ	YANINA VANESA G.'];
+  //novedad:string[]=['Compensatorio','L.A.O.','Maternidad','Parte de Enfermo','Familiar Enfermo','Falta sin aviso']
+  hospitales: any;
+  profesionales: any;
+  licencias: any;
+
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http
+      .get<any[]>('../assets/jsonFiles/hospitales.json')
+      .subscribe((data) => {
+        this.hospitales = data;
+      });
+
+      this.http
+      .get<any[]>('../assets/jsonFiles/profesionales.json')
+      .subscribe((data) => {
+        this.profesionales = data;
+      });
+
+      this.http
+      .get<any[]>('../assets/jsonFiles/licencias.json')
+      .subscribe((data) => {
+        this.licencias = data;
+      });
+
+
+  }
+
   /*
   selectedService: string = 'Compensatorio';
   selectedGuard: string = '';
