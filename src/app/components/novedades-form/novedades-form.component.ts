31a7554aa9ad2122202a90a427fb1d9070445408
fabiana Yucgra <fabianay0024@gmail.com>
2023-09-12 19:25:16 -0300

    subo cambios

--- src/app/components/novedades-form/novedades-form.component.ts
+++ src/app/components/novedades-form/novedades-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -9,7 +10,21 @@ import { Component } from '@angular/core';
 export class NovedadesFormComponent {
   hospitales:string[]= ['DN. PABLO SORIA'];
   profesional:string[]= ['FIGUEROA	ELIO','ARRAYA	PEDRO ADEMIR','MORALES	RICARDO','ALFARO	FIDEL','MARTINEZ	YANINA VANESA G.'];
-  novedad:string[]=['Compensatorio','L.A.O.','Maternidad','Parte de Enfermo','Familiar Enfermo','Falta sin aviso']
+  novedad:string[]=['Compensatorio','L.A.O.','Maternidad','Parte de Enfermo','Familiar Enfermo','Falta sin aviso'];
+  novedadesForm:FormGroup;
+
+  constructor(
+    private fb:FormBuilder,
+  ){
+    this.novedadesForm = this.fb.group({
+      hospital: ['', Validators.required],
+      profesional: ['', Validators.required],
+      novedad: ['', Validators.required],
+      fecInicio: ['', Validators.required],
+      fecFin: ['', Validators.required],
+
+    });
+  }
   /*
   selectedService: string = 'Compensatorio';
   selectedGuard: string = '';
