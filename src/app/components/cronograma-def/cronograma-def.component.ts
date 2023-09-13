be436bff419ecdf66e0f6a686b442b5ffe53e58e
sebcostilla <sebcostilla@gmail.com>
2023-09-13 12:14:12 -0300

    definitivo con json

--- src/app/components/cronograma-def/cronograma-def.component.ts
+++ src/app/components/cronograma-def/cronograma-def.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,4 +7,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./cronograma-def.component.css']
 })
 export class CronogramaDefComponent {
+
+
+  profesionales?: any[];
+  hospitales?: any[];
+  profesionalesFiltrados?: any[];
+  profesionalesContrafactura?: any[];
+  profesionalesCargo?: any[];
+  profesionalesExtra?: any[];
+  profesionalesReagrupacion?: any[];
+  profesionalesPasiva?: any[];
+
+  hospitalSeleccionado: any;
+  profesionalActual: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<any[]>('../assets/jsonFiles/hospitales.json')
+      .subscribe((data) => {
+        this.hospitales = data;
+        this.hospitalSeleccionado = this.hospitales[3];
+        this.filtrarProfesionalesPorHospital();
+      });
+
+    this.http
+      .get<any[]>('../assets/jsonFiles/profesionales.json')
+      .subscribe((data) => {
+        this.profesionales = data;
+      });
+
+    this.http
+      .get<any[]>('../assets/jsonFiles/profesionales.json')
+      .subscribe((data) => {
+        this.profesionales = data;
+        this.profesionalActual = this.profesionales[0];
+      });
+  }
+
+  seleccionarHospital(hospital: any) {
+    console.log('Hospital seleccionado:', hospital.descripcion);
+    this.profesionalesFiltrados = [];
+    this.hospitalSeleccionado = hospital;
+    this.filtrarProfesionalesPorHospital();
+  }
+
+  filtrarProfesionalesPorHospital() {
+    if (this.profesionales) {
+      this.profesionalesFiltrados = this.profesionales.filter((profesional) => {
+        console.log(this.profesionalesFiltrados);
+        return profesional.hospital == this.hospitalSeleccionado.descripcion;
+      });
+      this.filtrarPorTipoGuardia();
+    }
+  }
+
+  filtrarPorTipoGuardia(){
+    if (this.profesionalesFiltrados) {
+      this.profesionalesContrafactura = this.profesionalesFiltrados.filter((profesional) => {
+        return profesional.tipoGuardia == "contrafactura";
+      });
+      this.profesionalesCargo = this.profesionalesFiltrados.filter((profesional) => {
+        return profesional.tipoGuardia == "cargo";
+      });
+      this.profesionalesExtra = this.profesionalesFiltrados.filter((profesional) => {
+        return profesional.tipoGuardia == "extra";
+      });
+      this.profesionalesReagrupacion = this.profesionalesFiltrados.filter((profesional) => {
+        return profesional.tipoGuardia == "reagrupacion";
+      });
+
+      this.profesionalesPasiva = this.profesionalesFiltrados.filter((profesional) => {
+        return profesional.tipoGuardia == "pasiva";
+      });
+    }
+  }
 }
--- src/app/components/cronograma-reg/cronograma-reg.component.ts
+++ src/app/components/cronograma-reg/cronograma-reg.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cronograma-reg',
   templateUrl: './cronograma-reg.component.html',
   styleUrls: ['./cronograma-reg.component.css']
 })
-export class CronogramaRegComponent {
+export class CronogramaRegComponent  implements OnInit {
 
+  hospitales?: any[];
+  hospitalesFiltrados?: any[];
+  selectedRegion: string = 'CENTRO';
+  hospitalSeleccionado: any;
+  class?: string;
+
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<any[]>('../assets/jsonFiles/hospitales.json')
+      .subscribe((data) => {
+        this.hospitales = data;
+      this.filtrarHospitalPorRegion()
+      });
+  }
+
+
+  selectRegion(region: string) {
+    this.selectedRegion = region;
+    this.filtrarHospitalPorRegion()
+    console.log(this.hospitalesFiltrados);
+
+  }
+
+  filtrarHospitalPorRegion() {
+    if (this.hospitales) {
+      this.hospitalesFiltrados = this.hospitales.filter((hospital) => {
+        return hospital.region == this.selectedRegion;
+      });
+    }else{
+      console.log("nola");
+    }
+  }
 }
