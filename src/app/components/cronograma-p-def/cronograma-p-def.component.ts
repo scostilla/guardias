b8a9b25983dd589876d69f20cde5bf14b6f6ed4d
sebcostilla <sebcostilla@gmail.com>
2023-09-15 12:34:33 -0300

    definitivo pasivas con json

--- src/app/components/cronograma-p-def/cronograma-p-def.component.ts
+++ src/app/components/cronograma-p-def/cronograma-p-def.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./cronograma-p-def.component.css']
 })
 export class CronogramaPDefComponent {
+
+  profesionales?: any[] = [];
+  hospitales?: any[] = [];
+  profesionalesFiltrados?: any[] = [];
+  profesionalesPasiva?: any[] = [];
+
+  hospitalSeleccionado: any;
+  profesionalActual: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<any[]>('../assets/jsonFiles/hospitales.json')
+      .subscribe((data) => {
+        this.hospitales = data;
+        this.hospitales = this.hospitales.filter(hospital => hospital.pasivas === true);
+        this.hospitalSeleccionado = this.hospitales[1];
+        this.filtrarProfesionalesPorHospital();
+      });
+
+    this.http
+      .get<any[]>('../assets/jsonFiles/profesionales.json')
+      .subscribe((data) => {
+        this.profesionales = data;
+      });
+
+    this.http
+      .get<any[]>('../assets/jsonFiles/profesionales.json')
+      .subscribe((data) => {
+        this.profesionales = data;
+        this.profesionalActual = this.profesionales[0];
+      });
+  }
+
+  seleccionarHospital(hospital: any) {
+    console.log('Hospital seleccionado:', hospital.descripcion);
+    this.profesionalesFiltrados = [];
+    this.hospitalSeleccionado = hospital;
+    this.filtrarProfesionalesPorHospital();
+  }
+
+  filtrarProfesionalesPorHospital() {
+    if (this.profesionales) {
+      this.profesionalesFiltrados = this.profesionales.filter((profesional) => {
+        console.log(this.profesionalesFiltrados);
+        return profesional.hospital == this.hospitalSeleccionado.descripcion;
+      });
+      this.filtrarPorTipoGuardia();
+    }
+  }
+
+  filtrarPorTipoGuardia(){
+    if (this.profesionalesFiltrados) {
+      this.profesionalesPasiva = this.profesionalesFiltrados.filter((profesional) => {
+        return profesional.tipoGuardia == "pasiva";
+      });
+    }
+  }
+
+  getRowIndex(profesionalActual: any): number {
+    if (this.profesionalesPasiva && this.profesionalesPasiva.length > 0) {
+      return this.profesionalesPasiva.indexOf(profesionalActual) + 1;
+    }
+    return 0;
+  }
+
 
 }
