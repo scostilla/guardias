21b0dedaa9206c7bcdedc320b872863722f65789
sebcostilla <sebcostilla@gmail.com>
2023-10-02 10:05:26 -0300

    .

--- src/app/components/cronograma/cronograma.component.ts
+++ src/app/components/cronograma/cronograma.component.ts
@@ -20,6 +20,15 @@ export class CronogramaComponent {
   profesionales?: any[];
   hospitales?: any[];
   profesionalesFiltrados?: any[];
+
+  profDomingo: any[] = [];
+  profLunes: any[] = [];
+  profMartes: any[] = [];
+  profMiercoles: any[] = [];
+  profJueves: any[] = [];
+  profViernes: any[] = [];
+  profSabado: any[] = [];
+
   hospitalSeleccionado: any;
   profesionalActual: any;
   class?: string;
@@ -64,21 +73,170 @@ export class CronogramaComponent {
     if (this.profesionalesFiltrados) {
       for (let i = 0; i < this.profesionalesFiltrados.length; i++) {
         console.log(this.profesionalesFiltrados[i].tipoGuardia);
-        this.profesionalesFiltrados[i].tipoGuardia =
+        switch(this.profesionalesFiltrados[i].tipoGuardia){
+          case 'cargo':
+            this.profesionalesFiltrados[i].tipoGuardia =
           'calendar-dia--text circulo--' +
           this.profesionalesFiltrados[i].tipoGuardia;
+            if(this.profesionalesFiltrados[i].cargoDomingo){
+              this.profDomingo.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+            if(this.profesionalesFiltrados[i].cargoLunes){
+              this.profLunes.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+            if(this.profesionalesFiltrados[i].cargoMartes){
+              this.profMartes.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+            if(this.profesionalesFiltrados[i].cargoMiercoles){
+              this.profMiercoles.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+            if(this.profesionalesFiltrados[i].cargoJueves){
+              this.profJueves.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+            if(this.profesionalesFiltrados[i].cargoViernes){
+              this.profViernes.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+            if(this.profesionalesFiltrados[i].cargoSabado){
+              this.profSabado.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+          break;
+          case 'extra':
+            this.profesionalesFiltrados[i].tipoGuardia =
+          'calendar-dia--text circulo--' +
+          this.profesionalesFiltrados[i].tipoGuardia;
+
+            if(this.profesionalesFiltrados[i].extraDomingo){
+              console.log(this.profesionalesFiltrados[i]);
+              this.profDomingo.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+            if(this.profesionalesFiltrados[i].extraLunes){
+              console.log(this.profesionalesFiltrados[i]);
+              this.profLunes.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+            if(this.profesionalesFiltrados[i].extraMartes){
+              console.log(this.profesionalesFiltrados[i]);
+              this.profMartes.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+            if(this.profesionalesFiltrados[i].extraMiercoles){
+              console.log(this.profesionalesFiltrados[i]);
+              this.profMiercoles.push(this.profesionalesFiltrados[i]);
+              console.log(this.profMiercoles );
+              return;
+            }
+            if(this.profesionalesFiltrados[i].extraJueves){
+              console.log(this.profesionalesFiltrados[i]);
+              this.profJueves.push(this.profesionalesFiltrados[i]);
+
+              return;
+            }
+            if(this.profesionalesFiltrados[i].extraViernes){
+              console.log(this.profesionalesFiltrados[i]);
+              this.profViernes.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+            if(this.profesionalesFiltrados[i].extraSabado){console.log(this.profesionalesFiltrados[i]);
+              this.profSabado.push(this.profesionalesFiltrados[i]);
+              return;
+            }
+          break;
+          case 'agrupacion':
+            this.profesionalesFiltrados[i].tipoGuardia =
+          'calendar-dia--text circulo--' +
+          this.profesionalesFiltrados[i].tipoGuardia;
+          if(this.profesionalesFiltrados[i].agrupacionDomingo){
+            this.profDomingo.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].agrupacionLunes){
+            this.profLunes.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].agrupacionMartes){
+            this.profMartes.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].agrupacionMiercoles){
+            this.profMiercoles.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].agrupacionJueves){
+            this.profJueves.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].agrupacionViernes){
+            this.profViernes.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].agrupacionSabado){
+            this.profSabado.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          break;
+          case 'Contrafactura':
+            this.profesionalesFiltrados[i].tipoGuardia =
+          'calendar-dia--text circulo--' +
+          this.profesionalesFiltrados[i].tipoGuardia;
+          if(this.profesionalesFiltrados[i].contrafacturaDomingo){
+            this.profDomingo.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].contrafacturaLunes){
+            this.profLunes.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].contrafacturaMartes){
+            this.profMartes.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].contrafacturaMiercoles){
+            this.profMiercoles.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].contrafacturaJueves){
+            this.profJueves.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].contrafacturaViernes){
+            this.profViernes.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          if(this.profesionalesFiltrados[i].contrafacturaSabado){
+            this.profSabado.push(this.profesionalesFiltrados[i]);
+            return;
+          }
+          break;
+        }
       }
     }
+    console.log(this.profDomingo);
+
+
+
   }
 
   filtrarProfesionalesPorHospital() {
     if (this.profesionales) {
       this.profesionalesFiltrados = this.profesionales.filter((profesional) => {
-        console.log(this.profesionalesFiltrados);
         return profesional.hospital == this.hospitalSeleccionado.descripcion;
       });
+      console.log(this.profesionalesFiltrados);
     }
   }
+
+
+
+
+
 
   openPopupCalendario() {
     this.dialogReg.open(PopupCalendarioComponent, {
