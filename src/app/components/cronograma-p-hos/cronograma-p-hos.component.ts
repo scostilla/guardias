dc5455e3c1d4c7cc0d15d6e338ac4737fb1613dd
fabiyuc <fabianay0024@gmail.com>
2023-10-02 00:38:29 -0300

    prueba de tabla

--- src/app/components/cronograma-p-hos/cronograma-p-hos.component.ts
+++ src/app/components/cronograma-p-hos/cronograma-p-hos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient  } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./cronograma-p-hos.component.css']
 })
 export class CronogramaPHosComponent {
+  profesionales: any[] = [];
+  profesionalesTemp: any[] = [];
+  hospitales: any[] = [];
+  profesionalesLunes: any[] = [];
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.cargarProfesionales(); 
+
+  }
+  
+  cargarProfesionales(){
+    this.http.get<any[]>('../assets/jsonFiles/profesionalesPasivas.json').subscribe(data => {
+      this.profesionales = data;
+
+      this.cargarProfesionalLunes();
+    });
+  }
+
+  cargarProfesionalLunes(){
+    this.http.get<any[]>('../assets/jsonFiles/profesionalesPasivas.json').subscribe(data => {
+      this.profesionalesTemp = data;
+    
+      for(let i = 0 ; i <= this.profesionalesTemp.length; i++){
+        if(this.profesionalesTemp[i].id  >2)
+        this.profesionalesLunes.push(this.profesionales[i]) ;
+      }
+      console.log("pasear el json  "+ this.profesionalesLunes[0].nombre);
+      
+      console.log("faf  "+ this.profesionalesLunes.length);
+    });
+    
+  }
+  
 
 }
