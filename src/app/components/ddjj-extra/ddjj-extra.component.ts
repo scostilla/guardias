7b9806dcba15220c145e6c6cffcbb3a623e7e315
sebcostilla <sebcostilla@gmail.com>
2023-10-05 12:02:45 -0300

    .

--- src/app/components/ddjj-extra/ddjj-extra.component.ts
+++ src/app/components/ddjj-extra/ddjj-extra.component.ts
@@ -29,6 +29,8 @@ export class DdjjExtraComponent {
 
   profesionales: any[] = [];
   enableTotales: boolean = false;
+  horasSemanal: number = 0;
+  horasFindeSemana: number = 0;
   observ!: string;
 
   constructor(private route: ActivatedRoute, public dialog: MatDialog) {}
@@ -56,6 +58,40 @@ export class DdjjExtraComponent {
 
   btnPaseDPH(){
     this.enableTotales = true;
+
+    if(this.profesionales){
+      for (let i = 0; i < this.profesionales.length; i++) {
+
+        if(this.profesionales[i].extraLunes){
+          this.horasSemanal = + this.profesionales[i].extraLunes;
+        }
+        if(this.profesionales[i].extraMartes){
+          this.horasSemanal = + this.profesionales[i].extraMartes;
+        }
+        if(this.profesionales[i].extraMiercoles){
+          this.horasSemanal = + this.profesionales[i].extraMiercoles;
+        }
+        if(this.profesionales[i].extraJueves){
+          this.horasSemanal = + this.profesionales[i].extraJueves;
+        }
+        if(this.profesionales[i].extraViernes){
+          this.horasSemanal = + this.profesionales[i].extraViernes;
+        }
+
+        if(this.profesionales[i].extraSabado){
+          this.horasFindeSemana = + this.profesionales[i].extraSabado;
+        }
+        if(this.profesionales[i].extraDomingo){
+          this.horasFindeSemana = + this.profesionales[i].extraDomingo;
+        }
+
+        //SOLO PARA ESTER MES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //MULTIPLICO *4 MARTES A VIERNES Y *5 FINDE Y LUNES
+        this.profesionales[i].horasSemanal = this.horasSemanal*4+this.profesionales[i].extraLunes ||0;
+        this.profesionales[i].horasFindeSemana = this.horasFindeSemana*5 ||0;
+        this.profesionales[i].horasTotal = this.profesionales[i].horasSemanal + this.profesionales[i].horasFindeSemana;
+      }
+    }
   }
 
 }
