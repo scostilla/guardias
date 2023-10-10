b0b12f9b701c6c6bbeb5d5995539919d91ae1288
sebcostilla <sebcostilla@gmail.com>
2023-10-10 18:27:05 -0300

    .

--- src/app/components/ddjj-contrafactura/ddjj-contrafactura.component.ts
+++ src/app/components/ddjj-contrafactura/ddjj-contrafactura.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
+import { DialogData } from '../ddjj-cargoyagrup/ddjj-cargoyagrup.component';
 import { PopupDdjjCfEditComponent } from '../popup-ddjj-cf-edit/popup-ddjj-cf-edit.component';
 import { PopupDdjjCfComponent } from '../popup-ddjj-cf/popup-ddjj-cf.component';
 
@@ -11,6 +12,11 @@ import { PopupDdjjCfComponent } from '../popup-ddjj-cf/popup-ddjj-cf.component';
 })
 export class DdjjContrafacturaComponent {
   profesionales: any[] = [];
+  enableTotales: boolean = false;
+  enableAprobada: boolean = false;
+  enableRechazada: boolean = false;
+  estado: string = 'pendiente';
+  observ!: string;
 
   constructor(public dialogReg: MatDialog, private route: ActivatedRoute) {}
   openPopupCf() {
@@ -34,5 +40,30 @@ export class DdjjContrafacturaComponent {
       width: '550px',
       disableClose: true,
     });
+  }
+  btnAprobado() {
+    this.enableAprobada = true;
+    this.estado = 'aprobado';
+  }
+
+  btnRechazado() {
+    if (this.observ) {
+      this.enableRechazada = true;
+      this.estado = 'rechazado';
+    } else {
+      this.enableRechazada = false;
+      this.estado = 'pendiente';
+    }
+    console.log(this.enableRechazada);
+  }
+}
+export class DialogObserv {
+  constructor(
+    public dialogRef: MatDialogRef<DialogObserv>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
