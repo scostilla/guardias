504e2ac504b5318902faf6f90c6b2d0badb51b55
ErgoMori <nikho_@hotmail.com>
2023-12-06 20:26:19 -0300

    cambios dpto

--- src/app/components/configuracion/territorio/departamento-edit/departamento-edit.component.ts
+++ src/app/components/configuracion/territorio/departamento-edit/departamento-edit.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Departamento } from 'src/app/models/departamento';
+import { DepartamentoService } from 'src/app/services/departamento.service';
+
 
 @Component({
   selector: 'app-departamento-edit',
   templateUrl: './departamento-edit.component.html',
   styleUrls: ['./departamento-edit.component.css']
 })
-export class DepartamentoEditComponent {
+export class DepartamentoEditComponent implements OnInit {
 
-}
+  departamento?: Departamento;
+
+  constructor(
+    private departamentoService: DepartamentoService,
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router,
+    public dialogRef: MatDialogRef<DepartamentoEditComponent>,
+  @Inject(MAT_DIALOG_DATA) public data: { id: number }
+  ) {}
+
+  ngOnInit() {
+    const id = this.data.id;
+    this.departamentoService.detail(id).subscribe(
+      data=> {
+        this.departamento = data;
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Error', {
+          timeOut: 6000, positionClass: 'toast-top-center'
+        });
+      }
+    );
+  }
+
+  onUpdate(): void {
+    const id = this.data.id;
+    console.log("onUpdate "+id);
+    if(this.departamento) {
+    this.departamentoService.update(id, this.departamento).subscribe(
+      data=> {
+        this.toastr.success('Departamento Modificado', 'OK', {
+          timeOut: 7000, positionClass: 'toast-top-center'
+        });
+        this.router.navigate(['/departamento'])
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Error', {
+          timeOut: 7000, positionClass: 'toast-top-center'
+        });
+        this.router.navigate(['/departamento'])
+      }
+    )
+  }
+
+  this.dialogRef.close();
+
+  }
+
+  cancel() {
+    this.dialogRef.close();
+  }
+
+  }
+
