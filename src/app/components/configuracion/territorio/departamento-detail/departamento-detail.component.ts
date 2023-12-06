e36e15c770bb6e59e52c6474f8d0efdc427b6f14
ErgoMori <nikho_@hotmail.com>
2023-12-06 17:36:03 -0300

    cambios en dpto

--- src/app/components/configuracion/territorio/departamento-detail/departamento-detail.component.ts
+++ src/app/components/configuracion/territorio/departamento-detail/departamento-detail.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Departamento } from 'src/app/models/departamento';
+import { DepartamentoService } from 'src/app/services/departamento.service';
+
 
 @Component({
   selector: 'app-departamento-detail',
   templateUrl: './departamento-detail.component.html',
   styleUrls: ['./departamento-detail.component.css']
 })
-export class DepartamentoDetailComponent {
 
+export class DepartamentoDetailComponent implements OnInit {
+
+  departamento?: Departamento;
+
+  constructor(
+    private departamentoService: DepartamentoService,
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router,
+    public dialogo: MatDialogRef<DepartamentoDetailComponent>,
+  @Inject(MAT_DIALOG_DATA) public data: { id: number }
+
+  ) {}
+
+  ngOnInit() {
+    const id = this.data.id;
+    this.departamentoService.detail(id).subscribe(
+      data=> {
+        this.departamento = data;
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Error', {
+          timeOut: 6000, positionClass: 'toast-top-center'
+        });
+        this.dialogo.close();
+      }
+    );
+
+  }
+
+  cancel() {
+    this.dialogo.close();
+  }
 }
