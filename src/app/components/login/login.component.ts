399f5e67017d8d87c100182ace48dca6ec2e7ef6
fabiyuc <fabianay0024@gmail.com>
2024-03-25 15:52:53 -0300

    listo login ok

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,77 @@
-import { Component } from '@angular/core';
-import { AuthService } from 'src/app/services/auth/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { LoginUsuario } from 'src/app/models/login/login-usuario';
+import { AuthService } from 'src/app/services/login/auth.service';
+
+import { TokenService } from 'src/app/services/login/token.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  username!: string;
+export class LoginComponent implements OnInit {
+  
+  isLogged = false;
+  isLoginFail = false;
+  loginUsuario?: LoginUsuario;
+  nombreUsuario?: string;
+  password?: string;
+  roles: string[] =[];
+  errMsj?: string;
+
+  constructor(
+    private tokenService: TokenService,
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ){}
+  
+  ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+      this.isLoginFail = false;
+      this.roles = this.tokenService.getAuthorities();
+      console.log("roless" + this.roles);
+    }
+  }
+  onLogin(): void {
+    this.loginUsuario = new LoginUsuario(this.nombreUsuario!, this.password!);
+    console.log("Usuario " + this.nombreUsuario);
+    console.log("pass " + this.password);
+    this.authService.login(this.loginUsuario).subscribe(
+      data => {
+
+        this.isLogged = true;
+        console.log("isLogged " + this.isLogged);
+        this.tokenService.setToken(data.token);
+        console.log("Token " + data.token);
+        this.tokenService.setUserName(data.nombreUsuario);
+        console.log("userName " + data.nombreUsuario);
+        this.tokenService.setAuthorities(data.authorities);
+        console.log("Authorities " + data.authorities);
+        this.roles = data.authorities;
+        this.toastr.success('Bienvenido ' + data.nombreUsuario, 'OK', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+        this.router.navigate(['/']);
+      },
+      err => {
+        this.isLogged = false;
+        this.isLoginFail = true;
+        this.errMsj = err.error.message;
+        this.toastr.error(this.errMsj, 'Fail msj', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });
+        //console.log(err.error.message);
+      }
+    );
+  }
+
+  
+  /* username!: string;
   password!: string;
 
   constructor(private authService: AuthService) {}
@@ -20,6 +84,6 @@ export class LoginComponent {
     } else {
       console.log('Credenciales incorrectas');
     }
-  }
+  } */
 }
 
