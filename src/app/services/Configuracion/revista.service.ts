492b5548e9d95d9dbe22936bbecb1401830fc792
ErgoMori <nikho_@hotmail.com>
2024-07-17 16:20:45 -0300

    agrego cambios Fabi en develop

--- src/app/services/Configuracion/revista.service.ts
+++ src/app/services/Configuracion/revista.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { RevistaDto } from 'src/app/dto/Configuracion/RevistaDto';
 import { Revista } from "src/app/models/Configuracion/Revista";
 
 @Injectable({
@@ -14,38 +15,47 @@ export class RevistaService {
 
   constructor(private httpClient: HttpClient) { }
 
-  get refresh$(){
+  get refresh$() {
     return this._refresh$;
   }
 
   public list(): Observable<Revista[]> {
-      return this.httpClient.get<Revista[]>(this.revistasURL + 'list');
+    return this.httpClient.get<Revista[]>(this.revistasURL + 'list');
   }
 
-  public detail(id:number): Observable<Revista> {
-      return this.httpClient.get<Revista>(this.revistasURL + `detail/${id}`);
+  public detail(id: number): Observable<Revista> {
+    return this.httpClient.get<Revista>(this.revistasURL + `detail/${id}`);
   }
 
-public save(revistas:Revista): Observable<any> {
-  return this.httpClient.post<any>(this.revistasURL + 'create', revistas)
-  .pipe(
-    tap(() => {
-     this._refresh$.next();
-    })
-  )
-}
+  public checkRevista(revistaDto: RevistaDto): Observable<Revista> {
+    return this.httpClient.post<Revista>(this.revistasURL + `check`,revistaDto)
+    .pipe(
+      tap(() => {
+        this._refresh$.next();
+      })
+    )
+  }
 
-public update(id:number, revistas:Revista): Observable<any> {
-  return this.httpClient.put<any>(this.revistasURL + `update/${id}`, revistas)
-  .pipe(
-    tap(() => {
-     this._refresh$.next();
-    })
-  )
-}
+  public save(revista: RevistaDto): Observable<any> {
+    return this.httpClient.post<any>(this.revistasURL + 'create', revista)
+      .pipe(
+        tap(() => {
+          this._refresh$.next();
+        })
+      )
+  }
 
-public delete(id:number): Observable<any> {
-  return this.httpClient.put<any>(this.revistasURL + `delete/${id}`,{});
-}
+  public update(id: number, revistas: Revista): Observable<any> {
+    return this.httpClient.put<any>(this.revistasURL + `update/${id}`, revistas)
+      .pipe(
+        tap(() => {
+          this._refresh$.next();
+        })
+      )
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.httpClient.put<any>(this.revistasURL + `delete/${id}`, {});
+  }
 
 }
