3f6f0ba0af834b53fe1f2bb2dc44015c79fc00eb
sebcostilla <sebcostilla@gmail.com>
2024-07-28 17:59:01 -0300

    faceApiService funcionando

--- src/app/services/FaceApi/video-player.service.ts
+++ src/app/services/FaceApi/video-player.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { FaceApiService } from './face-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideoPlayerService {
 
-  constructor() { }
+  constructor(private faceApiService: FaceApiService) { }
+
+  getLandMark = (videoElement:any) => {
+const {globalFace} = this.faceApiService;
+  }
+
 }
