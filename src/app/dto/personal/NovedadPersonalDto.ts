6bae162b842a862f2c6c60746a63504d08a4dbd6
ErgoMori <nikho_@hotmail.com>
2024-09-23 16:11:53 -0300

    agrego novedades

---
+++ src/app/dto/personal/NovedadPersonalDto.ts
@@ -0,0 +1,41 @@
+export class NovedadPersonalDto {
+    idPersona: number;
+    idSuplente : number;
+    idArticulo: number;
+    idInciso: number;
+    fechaInicio: Date;
+    fechaFinal: Date;
+    puedeRealizarGuardia: boolean;
+    cobraSueldo: boolean;
+    necesitaReemplazo: boolean;
+    actual: boolean;
+    descripcion: string;
+
+
+
+    constructor(
+        idPersona: number,
+        idSuplente : number,
+        idArticulo: number,
+        idInciso: number,
+        fechaInicio: Date,
+        fechaFinal: Date,
+        puedeRealizarGuardia: boolean,
+        cobraSueldo: boolean,
+        necesitaReemplazo: boolean,
+        actual: boolean,
+        descripcion: string,
+        ){
+        this.idPersona = idPersona;
+        this.idSuplente = idSuplente;
+        this.idArticulo = idArticulo;
+        this.idInciso = idInciso;
+        this.fechaInicio = fechaInicio;
+        this.fechaFinal = fechaFinal;
+        this.puedeRealizarGuardia = puedeRealizarGuardia;
+        this.cobraSueldo = cobraSueldo;
+        this.necesitaReemplazo = necesitaReemplazo;
+        this.actual = actual;
+        this.descripcion = descripcion;
+    }
+}
