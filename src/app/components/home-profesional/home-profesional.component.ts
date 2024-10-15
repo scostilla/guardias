7f6860874bb364d99d6ba9aef861308f4bfd967a
Luis Guerrero <beto_lage16@hotmail.com>
2024-10-15 16:13:24 -0300

    .

--- src/app/components/home-profesional/home-profesional.component.ts
+++ src/app/components/home-profesional/home-profesional.component.ts
@@ -1,80 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { RegistroDiarioProfesionalComponent } from '../actividades/registro-diario-profesional/registro-diario-profesional.component';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { TokenService } from 'src/app/services/login/token.service';
-import { AuthService } from 'src/app/services/login/auth.service';
-import { PersonBasicPanelDto } from 'src/app/dto/person/PersonBasicPanelDto';
-import { EfectorSummaryDto } from 'src/app/dto/efector/EfectorSummaryDto';
+import { RegistroDiarioProfesionalComponent } from '../actividades/registro-diario-profesional/registro-diario-profesional.component';
 
 @Component({
   selector: 'app-home-profesional',
   templateUrl: './home-profesional.component.html',
   styleUrls: ['./home-profesional.component.css']
 })
-export class HomeProfesionalComponent implements OnInit {
+export class HomeProfesionalComponent {
 
   showRegistro: boolean = false;
   showAsistencia: boolean = false;
-  isLogged = false;
-  nombreUsuario: string = '';
-  apellidoUsuario: string = '';
-  nombresEfectores: EfectorSummaryDto[] = [];
-
-  constructor(
-    private router: Router,
-    public dialogReg: MatDialog,
-    private tokenService: TokenService,
-    private authService: AuthService
-  ) {}
-
-  ngOnInit(): void {
-    if (this.tokenService.getToken()) {
+  
+    isLogged = false;
+    constructor(
+      private router: Router,
+      public dialogReg: MatDialog,
+      private tokenService: TokenService
+      ) {}
+  
+    ngOnInit(): void {
+      if(this.tokenService.getToken()){
       this.isLogged = true;
+      }else {
+        this.isLogged = false;
+      }
+    }
+  
+    goToProfessionalForm() {
+      console.log('redirecting to professional form');
+      this.router.navigateByUrl('/professional-form');
+    }
 
-      // Obtener detalles del usuario
-      this.authService.detailPersonBasicPanel().subscribe(
-        (response: PersonBasicPanelDto) => {
-          this.nombreUsuario = response.nombre;
-          this.apellidoUsuario = response.apellido;
-          this.nombresEfectores = response.efectores; // Asignar efectores
+    toggleRegistro() {
+      this.showRegistro = !this.showRegistro;
+    }
+  
+    toggleAsistencia() {
+      this.showAsistencia = !this.showAsistencia;
+    }
 
-          // Log para mostrar el usuario y los efectores
-          console.log('Usuario logueado:', this.nombreUsuario, this.apellidoUsuario);
-          console.log('Efectores asociados:', this.nombresEfectores);
-        },
-        error => {
-          console.error('Error al obtener detalles del usuario:', error);
-        }
-      );
-    } else {
-      this.isLogged = false;
-      console.log('El usuario no está logueado.');
+    openRegistroDiarioProfesional(){
+      this.dialogReg.open(RegistroDiarioProfesionalComponent, {
+        width: '600px',
+        disableClose: true,
+      })
+    }  
+
+    onLogOut(): void{
+      this.tokenService.logOut();
+      window.location.reload();
     }
   }
-
-  goToProfessionalForm() {
-    console.log('Redirecting to professional form');
-    this.router.navigateByUrl('/professional-form');
-  }
-
-  toggleRegistro() {
-    this.showRegistro = !this.showRegistro;
-  }
-
-  toggleAsistencia() {
-    this.showAsistencia = !this.showAsistencia;
-  }
-
-  openRegistroDiarioProfesional() {
-    this.dialogReg.open(RegistroDiarioProfesionalComponent, {
-      width: '600px',
-      disableClose: true,
-    });
-  }
-
-  onLogOut(): void {
-    this.tokenService.logOut();
-    window.location.reload();
-  }
-}
+  
