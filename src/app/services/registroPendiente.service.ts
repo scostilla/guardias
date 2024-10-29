6750989a694e0df375d3cb996994c944139e63b9
ErgoMori <nikho_@hotmail.com>
2024-10-29 20:41:18 -0300

    completo service reg pendientes

--- src/app/services/registroPendiente.service.ts
+++ src/app/services/registroPendiente.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, Subject, tap } from "rxjs";
-import { RegistroMensual } from "../models/RegistroMensual";
+import { RegistroPendiente } from "../models/RegistroPendiente";
 
 @Injectable({
     providedIn: 'root'
@@ -17,13 +17,56 @@ import { RegistroMensual } from "../models/RegistroMensual";
       return this._refresh$;
     }
   
-    public list(): Observable<RegistroMensual[]> {
-        return this.httpClient.get<RegistroMensual[]>(this.registroMensualURL + 'list');
+    public list(): Observable<RegistroPendiente[]> {
+        return this.httpClient.get<RegistroPendiente[]>(this.registroMensualURL + 'list');
     }
   
-    public detail(id:number): Observable<RegistroMensual> {
-        return this.httpClient.get<RegistroMensual>(this.registroMensualURL + `detail/${id}`);
+    public detail(id:number): Observable<RegistroPendiente> {
+        return this.httpClient.get<RegistroPendiente>(this.registroMensualURL + `detail/${id}`);
     }
   
+    public detailByEfector(idEfector: number): Observable<RegistroPendiente[]> {
+      return this.httpClient.get<RegistroPendiente[]>(this.registroMensualURL + `detailByEfector/${idEfector}`);
+    }
+  
+    public detailByEfectorAndFecha(idEfector: number, mes: number, anio: number): Observable<RegistroPendiente[]> {
+      return this.httpClient.get<RegistroPendiente[]>(this.registroMensualURL + `detailByEfectorAndFecha/${idEfector}/${mes}/${anio}`);
+    }
+  
+    public create(registroPendiente: RegistroPendiente): Observable<any> {
+      return this.httpClient.post<any>(this.registroMensualURL + 'create', registroPendiente)
+        .pipe(
+          tap(() => {
+            this._refresh$.next();
+          })
+        );
+    }
+  
+    public update(id: number, registroPendiente: RegistroPendiente): Observable<any> {
+      return this.httpClient.put<any>(this.registroMensualURL + `update/${id}`, registroPendiente)
+        .pipe(
+          tap(() => {
+            this._refresh$.next();
+          })
+        );
+    }
+  
+    public delete(id: number): Observable<any> {
+      return this.httpClient.delete<any>(this.registroMensualURL + `delete/${id}`)
+        .pipe(
+          tap(() => {
+            this._refresh$.next();
+          })
+        );
+    }
+  
+    public deleteRegistroActividad(id: number, registroActividad: any): Observable<any> {
+      return this.httpClient.post<any>(this.registroMensualURL + `deleteRegistroActividad`, registroActividad)
+        .pipe(
+          tap(() => {
+            this._refresh$.next();
+          })
+        );
+    }
   
   }
