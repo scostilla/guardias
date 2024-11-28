52f7d49a561dcf3b6411436696404b11d1d3ef12
Luis Guerrero <beto_lage16@hotmail.com>
2024-11-27 21:36:35 -0300

    .

--- src/app/dto/Configuracion/CargoDto.ts
+++ src/app/dto/Configuracion/CargoDto.ts
@@ -1,31 +1,14 @@
 export class CargoDto{
     nombre: string;
     descripcion: string;
-    nroresolucion: string;
-    nrodecreto: string;
     activo: boolean;
-    fechaResolucion: Date;
-    fechaInicio: Date;
-    fechaFinal: Date;
     constructor(
       nombre: string,
       descripcion: string,
-      nroresolucion: string,
-      nrodecreto: string,
       activo: boolean,
-      fechaResolucion: Date,
-      fechaInicio: Date,
-      fechaFinal: Date,
     ) {
       this.nombre = nombre;
       this.descripcion = descripcion;
-      this.nroresolucion = nroresolucion;
-      this.nrodecreto = nrodecreto;
       this.activo = activo;
-      this.fechaResolucion = fechaResolucion;
-      this.fechaInicio = fechaInicio;
-      this.fechaFinal = fechaFinal;
-   
-    
     }
 }
