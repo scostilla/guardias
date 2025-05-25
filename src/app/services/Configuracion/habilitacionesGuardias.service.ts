70b6a53ae20d056b592777909330e504b080965a
fabiyuc <fabianay0024@gmail.com>
2025-05-25 12:41:10 -0300

    .

--- src/app/services/Configuracion/habilitacionesGuardias.service.ts
+++ src/app/services/Configuracion/habilitacionesGuardias.service.ts
@@ -19,72 +19,75 @@ export class HabilitacionesGuardiasService {
 
   constructor(private httpClient: HttpClient) { }
 
-  get refresh$(){
+  get refresh$() {
     return this._refresh$;
   }
 
   public list(): Observable<HabilitacionesGuardias[]> {
-      return this.httpClient.get<HabilitacionesGuardias[]>(this.permisosURL + 'list');
+    return this.httpClient.get<HabilitacionesGuardias[]>(this.permisosURL + 'list');
   }
 
   public listAll(): Observable<HabilitacionesGuardias[]> {
     return this.httpClient.get<HabilitacionesGuardias[]>(this.permisosURL + 'listAll');
-}
+  }
 
-  public detail(id:number): Observable<HabilitacionesGuardias> {
-      return this.httpClient.get<HabilitacionesGuardias>(this.permisosURL + `detail/${id}`);
+  public detail(id: number): Observable<HabilitacionesGuardias> {
+    return this.httpClient.get<HabilitacionesGuardias>(this.permisosURL + `detail/${id}`);
   }
 
   getPermisoByPersona(idAsistencial: number): Observable<HabilitacionesGuardias> {
     return this.httpClient.get<HabilitacionesGuardias>(`${this.permisosURL}detailAsistencial/${idAsistencial}`);
   }
 
-  public detailnombre(nombre:string): Observable<HabilitacionesGuardias> {
+  public detailnombre(nombre: string): Observable<HabilitacionesGuardias> {
     return this.httpClient.get<HabilitacionesGuardias>(this.permisosURL + `detailnombre/${nombre}`);
-}
+  }
 
-public save(permisos:HabilitacionesGuardiasDto): Observable<any> {
-  return this.httpClient.post<any>(this.permisosURL + 'create', permisos)
-  .pipe(
-    tap(() => {
-     this._refresh$.next(); 
-    })
-  )
-}
+  public save(permisos: HabilitacionesGuardiasDto): Observable<any> {
+    return this.httpClient.post<any>(this.permisosURL + 'create', permisos)
+      .pipe(
+        tap(() => {
+          this._refresh$.next();
+        })
+      )
+  }
 
-public update(id:number, permisos:HabilitacionesGuardiasDto): Observable<any> {
-  return this.httpClient.put<any>(this.permisosURL + `update/${id}`, permisos)
-  .pipe(
-    tap(() => {
-     this._refresh$.next(); 
-    })
-  )
-}
+  public update(id: number, permisos: HabilitacionesGuardiasDto): Observable<any> {
+    return this.httpClient.put<any>(this.permisosURL + `update/${id}`, permisos)
+      .pipe(
+        tap(() => {
+          this._refresh$.next();
+        })
+      )
+  }
 
-public delete(id:number): Observable<any> {
-  return this.httpClient.put<any>(this.permisosURL + `delete/${id}`, {});
-}
+  public delete(id: number): Observable<any> {
+    return this.httpClient.put<any>(this.permisosURL + `delete/${id}`, {});
+  }
 
-public deleteFisic(id:number): Observable<any> {
-  return this.httpClient.put<any>(this.permisosURL + `fisicdelete/${id}`, {});
-}
+  public deleteFisic(id: number): Observable<any> {
+    return this.httpClient.put<any>(this.permisosURL + `fisicdelete/${id}`, {});
+  }
 
-// Obtener habilitaciones por Efector y asistencial
-listHabilitacionesByEfector(idEfector: number): Observable<HabilitacionesGuardias[]> {
-  return this.httpClient.get<HabilitacionesGuardias[]>(`${this.permisosURL}listAsistencialesByEfector/${idEfector}`);
-}
+  // Obtener habilitaciones por Efector y asistencial
+  listHabilitacionesByEfector(idEfector: number): Observable<HabilitacionesGuardias[]> {
+    return this.httpClient.get<HabilitacionesGuardias[]>(`${this.permisosURL}listAsistencialesByEfector/${idEfector}`);
+  }
 
-// Verifica si un asistencial tiene permisos asociados a un efector
-tieneHabilitacionesGuardias(idAsistencial: number, idEfector: number): Observable<boolean> {
-  return this.httpClient.get<boolean>(`${this.permisosURL}tieneHabilitacionesGuardias/${idAsistencial}/${idEfector}`);
-}
+  // Verifica si un asistencial tiene permisos asociados a un efector
+  tieneHabilitacionesGuardias(idAsistencial: number, idEfector: number): Observable<boolean> {
+    return this.httpClient.get<boolean>(`${this.permisosURL}tieneHabilitacionesGuardias/${idAsistencial}/${idEfector}`);
+  }
 
-listAsistencialesByEfectorAndTG(idEfector: number, tipoGuardia: string): Observable<AsistencialSummaryDto[]> {
-  return this.httpClient.get<AsistencialSummaryDto[]>(`${this.permisosURL}listAsistencialesByEfectorAndTG/${idEfector}/${tipoGuardia}`);
-}
+  listAsistencialesByEfectorAndTG(idEfector: number, tipoGuardia: string): Observable<AsistencialSummaryDto[]> {
+    return this.httpClient.get<AsistencialSummaryDto[]>(`${this.permisosURL}listAsistencialesByEfectorAndTG/${idEfector}/${tipoGuardia}`);
+  }
 
-listAsistencialesWithCfAndExtraByEfector(idEfector: number): Observable<AsistencialListNombreTgDto[]> {
-  return this.httpClient.get<AsistencialListNombreTgDto[]>(`${this.permisosURL}listAsistencialesWithCfAndExtraByEfector/${idEfector}`);
-}
+  listAsistencialesWithCfAndExtraByEfector(idEfector: number): Observable<AsistencialListNombreTgDto[]> {
+    return this.httpClient.get<AsistencialListNombreTgDto[]>(`${this.permisosURL}listAsistencialesWithCfAndExtraByEfector/${idEfector}`);
+  }
 
+ /*  public listAsistencialesByEfectorAndTG(idEfector: number, tipoGuardia: string): Observable<AsistencialSummaryDto[]> {
+  return this.httpClient.get<AsistencialSummaryDto[]>(`${this.asistencialesURL}listAsistencialesByEfectorAndTG/${idEfector}/${tipoGuardia}`);
+} */
 }
